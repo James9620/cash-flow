fe83b7a6d554617965d6477d7a3ff4fe84a9d889
James Larkin <jinojai7@gmail.com>
2026-06-12 13:06:37 -0700

    Initial Commit

---
+++ cash-flow/cash_flowApp.swift
@@ -0,0 +1,17 @@
+//
+//  cash_flowApp.swift
+//  cash-flow
+//
+//  Created by James Larkin on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct cash_flowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
